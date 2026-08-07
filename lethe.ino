39c53150f44a6a922c6c7da45a0afbd62c5a01d8
Vulcan <logmedha09@gmail.com>
2026-08-07 16:16:41 +0100

    Initialize lethe.ino with button and pin definitions

---
+++ lethe.ino
@@ -0,0 +1,25 @@
+// buttons
+#define BUTTON_POWER D3
+#define BUTTON_HOME D4
+
+// i2c (for oled and imu)
+#define SDA_PIN D5
+#define SCL_PIN D6
+
+// imu interrupt
+#define IMU_INT1 D11
+
+// i2s (for amplifier)
+#define I2S_DOUT D12
+#define I2S_BCLK D13
+#define I2S_LRCLK D14;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
